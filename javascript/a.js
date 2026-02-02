740fb8afd88021e009c6d80fe53cc3e401ebbcc8
natalya2626 <natalyaivanova228@mail.ru>
2026-02-02 21:30:42 +0300

    javascript урок1

---
+++ javascript/a.js
@@ -0,0 +1,24 @@
+"use strict";
+const a = 3;
+const b = 7;
+const S = a * b;
+console.log(S);
+
+const logis = true == false;
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ javascript/conditions-html/conditions.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const number = +prompt("what is your age?");
+
+if (isNaN(age)) {
+  console.cog ("wrong input");
+} else {
+ if (age < 120) {
+ console.cog("Yor shall not pass");
+ } else {
+   console.log("you're welcome");
+  }
+ }
+
+
+
+// const number = +prompt("Input number");
+
+
+// if (number > 0) {
+//   alert("pozitive");
+// } else if (number < 0) {
+//  alert("negative");
+// } else {
+//   alert(0);
+// }
+
+
+
+// const number = +prompt("Input number");
+
+// if (number % 2 === 0) {
+//   alert("even");
+// } else {
+//   alert("odd");
+// }
+
+
+// const age = +(prompt("what is your age?"));
+
+// if (isNaN(age)) {
+//   console.cog ("wrong input");
+// } else {
+//  if (age < 18) {
+//  console.cog("Yor shall not pass");
+//  } else {
+//   console.log("you're welcome");
+//  }
+// }
